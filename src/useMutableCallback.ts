896a52168cdef5f1710471416e1211e785e648b2
Marik-D <maretskii@gmail.com>
2019-07-28 11:15:18 +0200

    Remove type cast to T

--- src/useMutableCallback.ts
+++ src/useMutableCallback.ts
@@ -1,7 +1,7 @@
 import { useMutable } from './useMutable'
 
-export function useMutableCallback<T extends (...args: any) => any> (callback: T): T {
+export function useMutableCallback<T extends (...args: any) => any>(callback: T) {
   const mutable = useMutable(callback)
   const mutableCallback = (...args: Parameters<T>): ReturnType<T> => mutable()(...args)
-  return mutableCallback as any
+  return mutableCallback
 }
