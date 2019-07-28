4b99162dea18a346fa396a52a1a4b743a946481c
Marik-D <maretskii@gmail.com>
2019-07-28 12:20:48 +0200

    Fix linter error

--- src/useMutableCallback.ts
+++ src/useMutableCallback.ts
@@ -1,6 +1,6 @@
 import { useMutable } from './useMutable'
 
-export function useMutableCallback<T extends (...args: any) => any>(callback: T) {
+export function useMutableCallback<T extends (...args: any) => any> (callback: T) {
   const mutable = useMutable(callback)
   const mutableCallback = (...args: Parameters<T>): ReturnType<T> => mutable()(...args)
   return mutableCallback
